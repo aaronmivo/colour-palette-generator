44d4e383f34d7defc9a8770969cddba4362fbd83
aaronmivo <aamivo93@gmail.com>
2022-04-14 16:39:45 -0700

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 
+dotenv.config();
 const app = express()
-
 const PORT = process.env.PORT || 3001 
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-app.get("/testing",(req, res) => {
-    res.send("Testing..")
-} )
+mongoose.connect(process.env.MONGO_CONNECT, {}, (err) => {
+    if(err){
+        return console.error(err)
+    }
+    console.log("Connected to MongoDB")
+})
