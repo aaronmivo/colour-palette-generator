86b47a38e3df04ed61aec3fb656801bb30e3b97c
aaronmivo <aamivo93@gmail.com>
2022-04-16 21:58:20 -0700

    Added Bcrypt hashing

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/userModel")
+const bcrypt = require("bcryptjs")
 
 router.post("/", async (req, res) => {
     try {
-    const {username, password, passwordVerify} = req.body
-    if(!username || !password || !passwordVerify){
-        return res.status(400).json({errorMessage: "Missing fields"})
+        const {username, password, passwordVerify} = req.body
+        if(!username || !password || !passwordVerify){
+            return res.status(400).json({errorMessage: "Missing fields"})
     }  
     if(password.length < 8){
         return res.status(400).json({errorMessage: "Password too short"})
@@ -20,6 +21,16 @@ router.post("/", async (req, res) => {
     if(existingUser){
         return res.status(400).json({errorMessage: "Username is already taken"})
     }
+
+    const hashingSalt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, hashingSalt)
+
+    const newUser = new User({
+        username,
+        passwordHash
+    })
+
+    const savedUser =  await newUser.save()
     } catch(err){
         console.error(err)
         res.status(500).send()
